f411227513784bdfc430be048318565b55c520b0
daehjeon <daehjeon@gmail.com>
2018-12-02 17:09:37 -0500

    Distance and containedInCircles fixed (working copy)

--- Matlab/containedInCircles.m
+++ Matlab/containedInCircles.m
@@ -1,21 +1,10 @@
-function p_inner = containedInCircles (p_intersect, circles)
-    p_inner = struct('x',{},'y',{});
-    num_of_inner = 1;
-    inner_flag = 1;
-    
-    for i = 1:length(p_intersect)
-        inner_flag = 1;
+function p_inner = containedInCircles (point, circles)
+    p_inner = true;
         
-        for j = 1:length(circles)
-            d = distance(p_intersect(i), circles(j));
-            if d > circles(j).r
-                inner_flag = 0;
-            end
+    for j = 1:length(circles)
+        d = distance(point, circles(j));
+        if d > circles(j).radius
+            p_inner = false;
         end
-        
-        if inner_flag == 1
-            p_inner(num_of_inner) = p_intersect(i);
-            num_of_inner = num_of_inner + 1;
-        end
-    end 
+    end
 end
